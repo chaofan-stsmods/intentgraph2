1303527248fc46cff2dbc6c42c066afc57061f42
Chaofan <herbix@163.com>
2026-03-29 01:08:02 +0800

    Add setting page

--- intentgraph-2/export.bat
+++ intentgraph-2/export.bat
@@ -1,4 +1,7 @@
-..\megadot-4.5.1-m.7-windows-x86_64-llvm-editor-csharp\MegaDot_v4.5.1-stable_mono_win64_console.exe --headless --export-pack "Windows Desktop" .\publish\intentgraph2.pck
+..\..\megadot-4.5.1-m.7-windows-x86_64-llvm-editor-csharp\MegaDot_v4.5.1-stable_mono_win64_console.exe --headless --export-pack "Windows Desktop" .\publish\intentgraph2.pck
 copy .\intentgraph2.json .\publish\intentgraph2.json
 copy .\publish\intentgraph2.pck "E:\Games\steamlib\steamapps\common\Slay the Spire 2\mods\intentgraph2.pck"
 copy .\intentgraph2.json "E:\Games\steamlib\steamapps\common\Slay the Spire 2\mods\intentgraph2.json"
+
+cd ..\intentgraph2baselib
+dotnet publish
