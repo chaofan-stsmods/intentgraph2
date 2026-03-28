9e3d147f13e962eb0bdd4d086a496d95e4d8e00a
Chaofan <herbix@163.com>
2026-03-29 00:53:43 +0800

    Update folder structure

---
+++ intentgraph2baselib/BaseLibHelper.cs
@@ -0,0 +1,23 @@
+﻿using BaseLib.Config;
+using Godot;
+using IntentGraph2.Crossovers;
+
+namespace IntentGraph2.BaseLib;
+
+public class BaseLibHelper : IBaseLibHelper
+{
+    public IntentGraphModConfig Config => HelperModConfig.BaseConfig;
+
+    public void RegisterConfig()
+    {
+        ModConfigRegistry.Register(IntentGraphMod.ModId, new HelperModConfig());
+    }
+}
+
+public class HelperModConfig : SimpleModConfig
+{
+    internal static IntentGraphModConfig BaseConfig { get; set; } = new IntentGraphModConfig();
+
+    [ConfigSection("Hotkey")]
+    public static Key ToggleIntentGraphKey { get => BaseConfig.ToggleIntentGraphKey; set => BaseConfig.ToggleIntentGraphKey = value; }
+}
