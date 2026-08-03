1c9d1dc5d7985936c3704cbbabdbb18b2e0d36f6
Chaofan <herbix@163.com>
2026-08-03 13:19:16 +0800

    Refine proj files and add support for act4heart mod

--- intentgraph2/export.bat
+++ intentgraph2/export.bat
@@ -1,7 +1,6 @@
-..\..\megadot-4.5.1-m.7-windows-x86_64-llvm-editor-csharp\MegaDot_v4.5.1-stable_mono_win64_console.exe --headless --export-pack "Windows Desktop" .\publish\intentgraph2.pck --verbose
+..\..\megadot-4.5.1-m.14-windows-x86_64-llvm-editor-csharp\MegaDot_v4.5.1-stable_mono_win64_console.exe --headless --export-pack "Windows Desktop" .\publish\intentgraph2.pck --verbose
 copy .\intentgraph2.json .\publish\intentgraph2.json
-copy .\publish\intentgraph2.pck "E:\Games\steamlib\steamapps\common\Slay the Spire 2\mods\intentgraph2.pck"
-copy .\intentgraph2.json "E:\Games\steamlib\steamapps\common\Slay the Spire 2\mods\intentgraph2.json"
+dotnet msbuild .\intentgraph2core.csproj -t:DeployGodotFiles
 
 cd ..\intentgraph2baselib
 dotnet publish
